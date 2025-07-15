b04209ed5170a78a9e14363d8ba5b4753f0182aa
MR-VL <cosmmo6@gmail.com>
2025-07-14 19:01:24 -0500

    [SQL] create posts table

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,15 +1,24 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE users (
-                       user_id UUID DEFAULT gen_random_uuid() NOT NULL,
-                       username VARCHAR(50) UNIQUE NOT NULL,
-                       email VARCHAR(100) UNIQUE NOT NULL,
-                       password TEXT NOT NULL,
-                       created_at TIMESTAMP DEFAULT NOW() NOT NULL ,
-                       consolidev_username VARCHAR(50) UNIQUE NOT NULL,
-                       consolidev_key VARCHAR(50) UNIQUE,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL ,
+    consolidev_username VARCHAR(50) UNIQUE NOT NULL,
+    consolidev_key VARCHAR(50) UNIQUE,
 
-                       CONSTRAINT UsersPK PRIMARY KEY (user_id)
+    CONSTRAINT UsersPK PRIMARY KEY (username)
 );
 
 
+CREATE TABLE posts (
+    post_id UUID DEFAULT gen_random_uuid() NOT NULL ,
+    title TEXT NOT NULL,
+    content TEXT NULL,
+    username VARCHAR(50) NOT NULL ,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL ,
+
+    CONSTRAINT PostsPK PRIMARY KEY (post_id),
+    CONSTRAINT PostsAuthorFK FOREIGN KEY (username) REFERENCES users(username)
+);
